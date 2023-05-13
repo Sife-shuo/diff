10c9abd565101354be4a430f49acdcb36846cac5
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-05-13 15:41:39 +0800

    Change cli files

--- cli.js
+++ cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const diff=require("./diff");
 if(process.argv[2]&&process.argv[3]){
     if(process.argv[4]&&process.argv[4].replaceAll("-","").replaceAll("/","").toLowerCase()=="f"){
